d0928cc121a3b696ffab0ead93c2a74c168941f4
Manoj Paladugu <mpa602@uclive.ac.nz>
2019-10-04 01:05:53 +1300

    update electron script to only open the electron app, keeping browser closed.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
-
+const isDev = require('electron-is-dev');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
@@ -17,7 +17,7 @@ function createWindow () {
     });
 
     // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
