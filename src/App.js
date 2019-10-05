aa6ff9afcc77ebdb3d6a18268689aa79b860c271
Jono <jono.davis66@gmail.com>
2019-10-05 20:32:14 +1300

    undo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import secrets from './secrets';
+// import secrets from './secrets';
 
 function App() {
   return (
